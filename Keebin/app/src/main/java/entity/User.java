325cce643a4bf94972e2af3da40d1ecf5c73d3e2
Kasper Roland Pagh <Kasper Roland Pagh>
2016-10-27 02:27:42 +0200

    some rest methods

--- Keebin/app/src/main/java/entity/User.java
+++ Keebin/app/src/main/java/entity/User.java
@@ -23,7 +23,103 @@ public class User
 
     private List<LoyaltyCard> loyaltyCards;
 
+    public int getDatabseId()
+    {
+        return databseId;
+    }
 
+    public void setDatabseId(int databseId)
+    {
+        this.databseId = databseId;
+    }
 
+    public String getFirstName()
+    {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public Date getBirthday()
+    {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday)
+    {
+        this.birthday = birthday;
+    }
+
+    public String getSex()
+    {
+        return sex;
+    }
+
+    public void setSex(String sex)
+    {
+        this.sex = sex;
+    }
+
+    public int getRoleId()
+    {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId)
+    {
+        this.roleId = roleId;
+    }
+
+    public boolean isSubscriber()
+    {
+        return isSubscriber;
+    }
+
+    public void setSubscriber(boolean subscriber)
+    {
+        isSubscriber = subscriber;
+    }
+
+    public int getSubscriberFreeCoffeeLeft()
+    {
+        return subscriberFreeCoffeeLeft;
+    }
+
+    public void setSubscriberFreeCoffeeLeft(int subscriberFreeCoffeeLeft)
+    {
+        this.subscriberFreeCoffeeLeft = subscriberFreeCoffeeLeft;
+    }
+
+    public List<LoyaltyCard> getLoyaltyCards()
+    {
+        return loyaltyCards;
+    }
+
+    public void setLoyaltyCards(List<LoyaltyCard> loyaltyCards)
+    {
+        this.loyaltyCards = loyaltyCards;
+    }
 }
